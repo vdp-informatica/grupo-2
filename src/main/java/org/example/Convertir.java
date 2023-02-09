0dcb1a3150f1918c87546d137c4ab33c87d64e22
PC-SOBREMESA\Guillermo <baixeras1@gmail.com>
2023-02-09 11:10:51 +0100

    Agrego la funcionalidad de convertir fichero XML a JSON

---
+++ src/main/java/org/example/Convertir.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class Convertir {
+        JAXBContext jaxbContext;
+        static String jsonData;
+        static StringWriter stringWriter;
+    {
+        try {
+            jaxbContext = JAXBContext.newInstance(Sucursales.class);
+
+            //Convertimos el fichero XML a Objeto
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            Sucursales sucursales = (Sucursales) unmarshaller.unmarshal(new File("sucursales.xml"));
+
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            stringWriter = new StringWriter();
+            marshaller.marshal(sucursales, stringWriter);
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            try {
+                //Le damos un formato bonito de Json para que sea fácil de leer para el usuario
+                jsonData = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(sucursales);
+
+                try {
+                    //Creamos el fichero Json mapeado de las clases Sucursales, Sucursal y Cuenta
+                    mapper.writeValue(new File("sucursales.json"), sucursales);
+
+
+                } catch (IOException e) {
+                    System.out.println("No se encuentra el fichero");
+                    throw new RuntimeException(e);
+                }
+            } catch (JsonProcessingException e) {
+                System.out.println("Error en el mapeo del Json, revisar formato");
+                throw new RuntimeException(e);
+            }
+        } catch (JAXBException ex) {
+            System.out.println("Error en la conversión del fichero a Objeto, revisar el mapeo");
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+}
